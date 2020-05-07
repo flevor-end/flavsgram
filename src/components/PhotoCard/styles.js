4dfffdb80a3403606d511a5854c0780ea0d4b50e
The Flav <fabianpb923@gmail.com>
2020-05-07 11:04:51 -0500

    react-avanzado-custonHooks

--- src/components/PhotoCard/styles.js
+++ src/components/PhotoCard/styles.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components'
 import { fadeIn } from '../../Styles/animation'
 
+export const Article = styled.article`
+min-height: 200px;
+`
 export const ImgWrapper = styled.div`
 border-radius: 10px;
 display: block;
